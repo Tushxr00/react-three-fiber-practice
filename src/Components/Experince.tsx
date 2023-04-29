6fa1c7fb2bf836c7e9c3dd4a9a64ad77a5f340f9
Tushxr01 <97872103+Tushxr00@users.noreply.github.com>
2023-04-29 22:29:53 +0530

    FEAT: added the orange sphere

--- src/Components/Experince.tsx
+++ src/Components/Experince.tsx
@@ -7,15 +7,14 @@ const Experince = () => {
         <torusKnotGeometry />
         <meshNormalMaterial />
       </mesh> */}
-      <mesh
-        // scale={[1, 1, 1]}\
-        position={[2.5, 0, 0]}
-        // rotation={[0, 3, 0]}
-        scale={1.5}
-      >
-        <sphereGeometry args={[1.5, 32, 32]} />
-        {/* <meshBasicMaterial args={[{ color: "red", wireframe: true }]} /> */}
-        <meshBasicMaterial color="mediumpurple" wireframe={true} />
+      <mesh position={[2.5, 0, 0]} scale={1.5}>
+        <boxGeometry scale={1.5} />
+
+        <meshBasicMaterial color="mediumpurple" wireframe={false} />
+      </mesh>
+      <mesh position={[-2.5, 0, 0]} scale={1.5}>
+        <sphereGeometry args={[0.7, 32, 32]} />
+        <meshBasicMaterial color="orange" wireframe />
       </mesh>
     </>
   );
