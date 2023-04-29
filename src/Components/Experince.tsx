7c89c2966a1498e37ba16ec063fc31fa2cf18d48
Tushxr01 <97872103+Tushxr00@users.noreply.github.com>
2023-04-29 22:59:40 +0530

    FEAT: practice animation on the mesh

--- src/Components/Experince.tsx
+++ src/Components/Experince.tsx
@@ -1,21 +1,31 @@
-import React from "react";
+import { MeshProps, ThreeElements, useFrame } from "@react-three/fiber";
+import React, { useRef } from "react";
 
 const Experince = () => {
+  const groupRef = useRef();
+  const cubeRef = useRef(null);
+  useFrame((state, delta) => {
+    // console.log({ delta });
+    groupRef.current.rotation.y += delta;
+    // cubeRef.current.rotation.y += delta;
+  });
   return (
     <>
       {/* <mesh>
         <torusKnotGeometry />
         <meshNormalMaterial />
       </mesh> */}
-      <mesh position={[2.5, 0, 0]} scale={1.5}>
-        <boxGeometry scale={1.5} />
+      <group ref={groupRef}>
+        <mesh position={[2.5, 0, 0]} scale={1.5} ref={cubeRef}>
+          <boxGeometry scale={1.5} />
 
-        <meshBasicMaterial color="mediumpurple" wireframe={false} />
-      </mesh>
-      <mesh position={[-2.5, 0, 0]} scale={1.5}>
-        <sphereGeometry args={[0.7, 32, 32]} />
-        <meshBasicMaterial color="orange" wireframe />
-      </mesh>
+          <meshBasicMaterial color="mediumpurple" wireframe={false} />
+        </mesh>
+        <mesh position={[-2.5, 0, 0]} scale={1.5}>
+          <sphereGeometry args={[0.7, 32, 32]} />
+          <meshBasicMaterial color="orange" wireframe />
+        </mesh>
+      </group>
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
         <meshBasicMaterial color="greenYellow" />
