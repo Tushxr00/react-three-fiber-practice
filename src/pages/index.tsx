4e7144a8f794cc37c072609f0f1e6ef134e5d831
Tushxr01 <97872103+Tushxr00@users.noreply.github.com>
2023-04-30 20:50:33 +0530

    FEAT: added removed the orhtograpgic camera

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,16 @@ import Experince from "@/Components/Experince";
 import { Canvas } from "@react-three/fiber";
 
 export default function Home() {
+  const cameraSettings = {
+    fov: 45,
+    // zoom: 30,
+    near: 0.1,
+    far: 200,
+    position: [3, 2, 6],
+  };
   return (
     <div className="w-screen h-screen ">
-      <Canvas
-        orthographic
-        camera={{
-          fov: 45,
-          zoom: 30,
-          near: 0.1,
-          far: 200,
-          position: [3, 2, 6],
-        }}
-      >
+      <Canvas camera={cameraSettings}>
         <Experince />
       </Canvas>
     </div>
