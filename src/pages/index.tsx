89b771a81cc67e574e07cfb1852abb972c5247b9
Tushxr01 <97872103+Tushxr00@users.noreply.github.com>
2023-04-30 20:48:39 +0530

    FEAT: added orthographic camera

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,16 @@ import { Canvas } from "@react-three/fiber";
 export default function Home() {
   return (
     <div className="w-screen h-screen ">
-      <Canvas>
+      <Canvas
+        orthographic
+        camera={{
+          fov: 45,
+          zoom: 30,
+          near: 0.1,
+          far: 200,
+          position: [3, 2, 6],
+        }}
+      >
         <Experince />
       </Canvas>
     </div>
