0b392db58559e4ad0024c5a255879e1fa205dff9
Tushxr01 <97872103+Tushxr00@users.noreply.github.com>
2023-04-30 20:58:21 +0530

    FEAT: added camera animation

--- src/Components/Experince.tsx
+++ src/Components/Experince.tsx
@@ -9,16 +9,23 @@ extend({ OrbitControls });
 const Experince = () => {
   const groupRef = useRef();
   const cubeRef = useRef(null);
-  const { camera, gl } = useThree();
+  // const { camera, gl } = useThree();
   useFrame((state, delta) => {
+    // console.log(state.camera);
+    // console.log(state.clock.elapsedTime);
+    const angel = state.clock.elapsedTime;
+    state.camera.position.x = Math.sin(angel) * 8;
+    state.camera.position.z = Math.cos(angel) * 8;
+    state.camera.lookAt(0, 0, 0);
     // console.log({ delta });
+
     // groupRef.current.rotation.y += delta;
     cubeRef.current.rotation.y += delta;
   });
 
   return (
     <>
-      <orbitControls args={[camera, gl.domElement]} />
+      {/* <orbitControls args={[camera, gl.domElement]} /> */}
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
       {/* <mesh>
