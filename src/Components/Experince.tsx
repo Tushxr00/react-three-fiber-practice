086b8e40bd37bab667eb6b74269781d192cd5bb5
Tushxr01 <97872103+Tushxr00@users.noreply.github.com>
2023-05-07 01:27:21 +0530

    FIX: Removed the previous setup

--- src/Components/Experince.tsx
+++ src/Components/Experince.tsx
@@ -42,64 +42,52 @@ const Experince = () => {
       <mesh
         // position={[2.5, 0, 0]}
         scale={1}
-        position-x={4}
+        position-x={2}
         ref={cubeRef}
       >
         <boxGeometry scale={1.5} />
 
         <meshStandardMaterial color="mediumpurple" wireframe={false} />
       </mesh>
-      <TransformControls object={cubeRef} mode="translate" />
+      {/* <TransformControls object={cubeRef} mode="translate" /> */}
       {/*  */}
 
       {/*  */}
-      <PivotControls
+      {/* <PivotControls
         anchor={[0, 0, 0]}
         depthTest={false}
         lineWidth={4}
         axisColors={["#9381ff", "#ff4d6d", "#7ae582"]}
         scale={50}
         fixed={true}
-      >
-        <mesh position={[-4, 0, 0]} scale={1} ref={sphereRef}>
-          <sphereGeometry args={[0.7, 32, 32]} />
-          <meshStandardMaterial color="orange" wireframe={false} />
-          <Html
-            position={[1, 1, 0]}
-            wrapperClass="label"
-            center
-            distanceFactor={6}
-            // occlude={[sphereRef, cubeRef]}
-          >
-            {`That's a sphere`}
-          </Html>
-        </mesh>
-      </PivotControls>
+      > */}
+      <mesh position={[-2, 0, 0]} scale={1} ref={sphereRef}>
+        <sphereGeometry args={[0.7, 32, 32]} />
+        <meshStandardMaterial color="orange" wireframe={false} />
+        <Html
+          position={[1, 1, 0]}
+          wrapperClass="label"
+          center
+          distanceFactor={6}
+          // occlude={[sphereRef, cubeRef]}
+        >
+          {`That's a sphere`}
+        </Html>
+      </mesh>
+      {/* </PivotControls> */}
       {/*  */}
       {/* </group> */}
-      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={20}>
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
-        {/* <meshStandardMaterial color="greenYellow" /> */}
-        <MeshReflectorMaterial
+        <meshStandardMaterial color="greenYellow" />
+        {/* <MeshReflectorMaterial
           color="greenYellow"
           mirror={0.75}
           resolution={512}
           blur={[1000, 1000]}
           mixBlur={1}
-        />
+        /> */}
       </mesh>
-      <Float speed={5} floatIntensity={10}>
-        <Text
-          fontSize={1}
-          color={"salmon"}
-          position={[0, 2, 0]}
-          maxWidth={3}
-          textAlign="center"
-        >
-          I LOVE R3F
-          {/* <meshNormalMaterial side={THREE.DoubleSide} /> */}
-        </Text>
-      </Float>
     </>
   );
 };
